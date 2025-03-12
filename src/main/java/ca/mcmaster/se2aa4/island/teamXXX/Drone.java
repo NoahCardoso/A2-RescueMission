ce248b4478b1e7a6cccb5edf9a812d0a7197f78d
wongm135 <wongm135@mcmaster.ca>
2025-03-12 17:59:42 -0400

    Made a basic land search algorithm

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -2,29 +2,65 @@ package ca.mcmaster.se2aa4.island.teamXXX;
 
 class Drone{
 
-    private int x;
-    private int y;
+    private int x = 0;
+    private int y = 0;
     private int battery;
-    private char dir; 
+    private char lastDir;
+    private char dir;
+    private int lastScan = 0;
 
-    Drone(int battery){
+    Drone(int battery, char dir){
         this.battery = battery;
+        this.dir = dir;
     }
 
     public void setBattery(int battery){
         this.battery = battery;
     }
 
+    public void subBattery(int val) {
+        this.battery -= val;
+    }
+
     public int getBattery(){
         return this.battery;
     }
 
-    public int getDir(){
+    public char getDir(){
         return this.dir;
     }
 
     public void setDir(char dir){
+        this.lastDir = this.dir;
         this.dir = dir;
+    }
+
+    public char getLastDir(){
+        return this.lastDir;
+    }
+
+    public int getLastScan(){
+        return this.lastScan;
+    }
+
+    public void incLastScan(){
+        this.lastScan++;
+    }
+
+    public void resetLastScan() {
+        this.lastScan = 0;
+    }
+
+    public void updatePosition() {
+        if (dir == 'E'){
+            x++;
+        } else if (dir == 'W') {
+            x--;
+        } else if (dir == 'N') {
+            y--;
+        } else {
+            y++;
+        }
     }
 
     public int getX(){
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -12,6 +12,7 @@ import org.json.JSONTokener;
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+    private Drone drone;
     private JSONObject result = null;
     private String direction = null;
     private int x = 1;
@@ -23,6 +24,12 @@ public class Explorer implements IExplorerRaid {
     private char dir = 'E';
     private int n = 0;
     private boolean echo = true;
+    private String[] actions = {"scan", "echo", "fly", "heading"};
+    private int action = -1;
+    private boolean foundE = false;
+    private boolean foundC = false;
+    private boolean overOcean = false;
+    private int triggerTurn = 0;
 
     @Override
     public void initialize(String s) {
@@ -32,6 +39,7 @@ public class Explorer implements IExplorerRaid {
         String direction = info.getString("heading");
         
         Integer batteryLevel = info.getInt("budget");
+        drone = new Drone(batteryLevel, 'E');
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
     }
@@ -40,16 +48,27 @@ public class Explorer implements IExplorerRaid {
     public String takeDecision() {
         
         JSONObject decision = new JSONObject();
-        decision = findLand(decision);
+        if (n == 0) {
+            decision = findLand(decision);
+        } else if (triggerTurn == 0) {
+            decision = searchLandDown(decision);
+        } else if (1 <= triggerTurn && triggerTurn <= 3) {
+            decision = turn(decision);
+        } else {
+            decision = searchLandUp(decision);
+        }
+
+        if (foundE && foundC) {
+            decision = end(decision);
+        }
+
+        logger.info("** TriggerVal: {}",triggerTurn);
         logger.info("** Decision: {}",decision.toString());
         return decision.toString();
     }
 
     private JSONObject findLand(JSONObject decision){
-        
-        if (n == 1){
-            decision.put("action","stop");
-        }
+
         //find boarders
         if (limitX == 0 && limitY == 0){
             if(result == null){
@@ -72,14 +91,18 @@ public class Explorer implements IExplorerRaid {
             if(x < limitX/2){
                 if(x < limitX/2 - 1){
                     x++;
+                    drone.updatePosition();
                     decision.put("action", "fly");
                     return decision;
                 }
                 x++;
                 if(x == limitX/2){
+                    drone.updatePosition();
                     decision.put("action", "heading");
                     decision.put("parameters", new JSONObject().put("direction", "S"));
                     dir = 'S';
+                    drone.setDir('S');
+                    drone.updatePosition();
                     y++;
                 }
                 return decision;
@@ -100,12 +123,14 @@ public class Explorer implements IExplorerRaid {
             else if(dstY > 0){
                 y++;
                 dstY--;
+                drone.updatePosition();
                 decision.put("action","fly");
                 return decision;
             }
             else{
                 if( n == 0){
                     decision.put("action","scan");
+                    action = 0;
                     n = 1;
                 }
                 
@@ -118,17 +143,229 @@ public class Explorer implements IExplorerRaid {
         return decision;
     }
 
+    private JSONObject searchLandDown(JSONObject decision){
+        JSONObject info = result.getJSONObject("extras");
+
+        if (drone.getDir() == 'S') {
+            if (overOcean == true) {
+                triggerTurn = 1;
+                action = 2;
+                drone.updatePosition();
+                decision.put("action","fly");
+                return decision;
+            }
+
+            overOcean = true;
+
+            if (drone.getX() < limitX/2) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "E"));
+                drone.setDir('E');
+                drone.updatePosition();
+            } else {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "W"));
+                drone.setDir('W');
+                drone.updatePosition();
+            }
+            return decision;
+        }
+
+        if (info.has("biomes")) {
+            JSONArray biomes = info.getJSONArray("biomes");
+
+            if (!(biomes.getString(0).equals("OCEAN"))) {
+                overOcean = false;
+            }
+
+
+            if (drone.getDir() == 'E' && drone.getX() >= limitX/2 && biomes.length() == 1 && biomes.getString(0).equals("OCEAN")) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "S"));
+                drone.setDir('S');
+                drone.updatePosition();
+                return decision;
+            }
+
+            if (drone.getDir() == 'W' && drone.getX() < limitX/2 && biomes.length() == 1 && biomes.getString(0).equals("OCEAN")) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "S"));
+                drone.setDir('S');
+                drone.updatePosition();
+                return decision;
+            }
+        }
+
+        if (action == 3 || action == 2) {
+            action = 0;
+            decision.put("action","scan");
+            return decision;
+        } else {
+            action = 2;
+            drone.updatePosition();
+            decision.put("action","fly");
+            return decision;
+        }
+    }
+
+    private JSONObject searchLandUp(JSONObject decision){
+        JSONObject info = result.getJSONObject("extras");
+
+        if (drone.getDir() == 'N') {
+            if (drone.getX() < limitX/2) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "E"));
+                drone.setDir('E');
+                drone.updatePosition();
+            } else {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "W"));
+                drone.setDir('W');
+                drone.updatePosition();
+            }
+            return decision;
+        }
+
+        if (info.has("biomes")) {
+            JSONArray biomes = info.getJSONArray("biomes");
+
+            if (drone.getDir() == 'E' && drone.getX() >= limitX/2 && biomes.length() == 1 && biomes.getString(0).equals("OCEAN")) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "N"));
+                drone.setDir('N');
+                drone.updatePosition();
+                return decision;
+            }
+
+            if (drone.getDir() == 'W' && drone.getX() < limitX/2 && biomes.length() == 1 && biomes.getString(0).equals("OCEAN")) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "N"));
+                drone.setDir('N');
+                drone.updatePosition();
+                return decision;
+            }
+        }
+
+        if (action == 3 || action == 2) {
+            action = 0;
+            decision.put("action","scan");
+            return decision;
+        } else {
+            action = 2;
+            drone.updatePosition();
+            decision.put("action","fly");
+            return decision;
+        }
+    }
+
+    private JSONObject turn(JSONObject decision) {
+        if (triggerTurn == 1) {
+            if (drone.getX() < limitX/2) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "E"));
+                drone.setDir('E');
+                drone.updatePosition();
+            } else {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "W"));
+                drone.setDir('W');
+                drone.updatePosition();
+            }
+            triggerTurn++;
+            return decision;
+        } else if (triggerTurn == 2) {
+            action = 3;
+            drone.updatePosition();
+            decision.put("action", "heading");
+            decision.put("parameters", new JSONObject().put("direction", "N"));
+            drone.setDir('N');
+            drone.updatePosition();
+            triggerTurn++;
+            return decision;
+        } else {
+            if (drone.getX() < limitX/2) {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "E"));
+                drone.setDir('E');
+                drone.updatePosition();
+            } else {
+                action = 3;
+                drone.updatePosition();
+                decision.put("action", "heading");
+                decision.put("parameters", new JSONObject().put("direction", "W"));
+                drone.setDir('W');
+                drone.updatePosition();
+            }
+            triggerTurn++;
+            return decision;
+        }
+    }
+
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
         this.result = response;
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
+        drone.setBattery(drone.getBattery()-cost);
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
+        logger.info("Battery: {}", drone.getBattery());
+        logger.info("X: {}", drone.getX());
+        logger.info("Y: {}", drone.getY());
+        logger.info("LY: {}", limitY);
+        logger.info("LX: {}", limitX);
+        logData(extraInfo);
+    }
+
+    private void logData(JSONObject data) {
+        JSONArray creeks;
+        JSONArray eSites;
+
+        if (data.has("creeks")) {
+            creeks = data.getJSONArray("creeks");
+            if (creeks.length() >= 1) {
+                foundC = true;
+                logger.info("FOUND CREEK");
+            }
+        }
+        if (data.has("sites")) {
+            eSites = data.getJSONArray("sites");
+            if (eSites.length() >= 1) {
+                foundE = true;
+                logger.info("FOUND SITE");
+            }
+        }
+    }
+
+    private JSONObject end(JSONObject decision){
+        decision.put("action","stop");
+        return decision;
     }
 
     @Override
